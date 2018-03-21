87e6412ce71618632067b448956147fb4445b68f
Oz Mercer <ozmercer@gmail.com>
2018-03-21 12:30:15 +0200

    Add canvas area and connect to meme gallery

--- js/sprint2.js
+++ js/sprint2.js
@@ -1,42 +1,56 @@
 'use strict'
 console.log('File Loaded');
 
-var gImgs = [];
+var gMemes = [];
 var gCurrId = 1;
+var gCurrMeme;
 
-function getImg (url, keyWords){
-    var img = {
+function getMeme (url, keyWords, desc){
+    var meme = {
         id: gCurrId++,
         url: url,
-        keyWords: keyWords
+        keyWords: keyWords,
+        desc: desc,
     }
-    gImgs.push(img)
+    gMemes.push(meme)
 }
 
-getImg('../img/memes/1.png', ['high','stupid', 'happy', 'man', 'drunk', 'funny'])
-getImg('../img/memes/2.png', ['dog','look','funny'])
-getImg('../img/memes/3.png', ['cartoon','angry','man','why'])
-getImg('../img/memes/4.png', ['cartoon','man', 'futurama','think','thinking'])
-getImg('../img/memes/5.png', ['man','satisfied','cheers','drink','tuxedo','rich'])
-getImg('../img/memes/6.png', ['cartoon','comics','angry','funny','work'])
-getImg('../img/memes/7.png', ['cartoon','comics','angry','funny','work'])
-getImg('../img/memes/8.png', ['cartoon','comics','angry','funny','work'])
-getImg('../img/memes/9.png', ['cartoon','comics','angry','funny','work'])
-getImg('../img/memes/10.png', ['cartoon','comics','angry','funny','work'])
+getMeme('../img/memes/1.png', ['high','stupid', 'happy', 'man', 'drunk', 'funny'],'10 Guy')
+getMeme('../img/memes/2.png', ['dog','look','funny'],'Doge')
+getMeme('../img/memes/3.png', ['cartoon','angry','man','why'],'Y U No')
+getMeme('../img/memes/4.png', ['cartoon','man', 'futurama','think','thinking'],'Futurama Fry')
+getMeme('../img/memes/5.png', ['man','satisfied','cheers','drink','tuxedo','rich'],'Leonardo Dicaprio Cheers')
+getMeme('../img/memes/6.png', ['cartoon','comics','angry','funny','work'],'Boardroom Meeting Suggestion')
+getMeme('../img/memes/7.png', ['cartoon','comics','angry','funny','work'],'Batman Slapping Robin')
+getMeme('../img/memes/8.png', ['cartoon','comics','angry','funny','work'],'Oprah You Get A')
+getMeme('../img/memes/9.png', ['cartoon','comics','angry','funny','work'],'X, X Everywhere')
+getMeme('../img/memes/10.png', ['cartoon','comics','angry','funny','work'],'Roll Safe Think About It')
 
 function renderGallery() {
-    gImgs.forEach(function(img) {
-        renderImg(img);
+    gMemes.forEach(function(meme) {
+        renderMeme(meme, '.memes');
     })
-
 }
 
-function renderImg(img) {
-    var elMemes = document.querySelector('.memes');
+function renderMeme(meme, selector,clear) {
+    var elMemes = document.querySelector(selector);
     var newHtml = `
-        <div class="meme" id="meme${img.id}">
-            <img src="${img.url}">
+        <div class="meme" id="${meme.id}" onclick="openEditor(${meme.id})" href="#editor">
+            <a href="#editor"><img src="${meme.url}" alt="${meme.desc}"></a>
         </div>
-    `
-    elMemes.innerHTML += newHtml;
+    `;
+    if (clear) elMemes.innerHTML = newHtml
+    else elMemes.innerHTML += newHtml;
+}
+
+function openEditor(memeId) {
+    if (!gCurrMeme || gCurrMeme.id === memeId) {
+        // toggle only if first meme, or same meme selected twice
+        var elEditor = document.querySelector('.editor');
+        elEditor.classList.toggle('hide');
+    } 
+    gCurrMeme = gMemes.find(function(meme) {
+        return meme.id === memeId
+    })
+    renderMeme(gCurrMeme,'.canvas',true)
 }
