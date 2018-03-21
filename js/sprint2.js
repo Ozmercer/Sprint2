3b33b6d58dfe1c3fcd08d1a8e8a4f7952fe94e6b
Oz Mercer <ozmercer@gmail.com>
2018-03-21 11:00:37 +0200

    Add renderGalery func

--- js/sprint2.js
+++ js/sprint2.js
@@ -13,13 +13,30 @@ function getImg (url, keyWords){
     gImgs.push(img)
 }
 
-getImg('../img/1.png', ['high','stupid', 'happy', 'man', 'drunk', 'funny'])
-getImg('../img/2.png', ['dog','look','funny'])
-getImg('../img/3.png', ['cartoon','angry','man','why'])
-getImg('../img/4.png', ['cartoon','man', 'futurama','think','thinking'])
-getImg('../img/5.png', ['man','satisfied','cheers','drink','tuxedo','rich'])
-getImg('../img/6.png', ['cartoon','comics','angry','funny','work'])
-getImg('../img/7.png', ['cartoon','comics','angry','funny','work'])
-getImg('../img/8.png', ['cartoon','comics','angry','funny','work'])
-getImg('../img/9.png', ['cartoon','comics','angry','funny','work'])
-getImg('../img/10.png', ['cartoon','comics','angry','funny','work'])
+getImg('../img/memes/1.png', ['high','stupid', 'happy', 'man', 'drunk', 'funny'])
+getImg('../img/memes/2.png', ['dog','look','funny'])
+getImg('../img/memes/3.png', ['cartoon','angry','man','why'])
+getImg('../img/memes/4.png', ['cartoon','man', 'futurama','think','thinking'])
+getImg('../img/memes/5.png', ['man','satisfied','cheers','drink','tuxedo','rich'])
+getImg('../img/memes/6.png', ['cartoon','comics','angry','funny','work'])
+getImg('../img/memes/7.png', ['cartoon','comics','angry','funny','work'])
+getImg('../img/memes/8.png', ['cartoon','comics','angry','funny','work'])
+getImg('../img/memes/9.png', ['cartoon','comics','angry','funny','work'])
+getImg('../img/memes/10.png', ['cartoon','comics','angry','funny','work'])
+
+function renderGallery() {
+    gImgs.forEach(function(img) {
+        renderImg(img);
+    })
+
+}
+
+function renderImg(img) {
+    var elGallery = document.querySelector('.galley-container');
+    var newHtml = `
+        <div id="img${img.id}">
+            <img src="${img.url}">
+        </div>
+    `
+    elGallery.innerHTML += newHtml;
+}
